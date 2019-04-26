0ced8e6be24438840a5b1f08a261317085953a69
bpadhiyar4 <bpadhiyar4@gmail.com>
2019-04-26 18:58:45 +0530

    fix nav issue

---
+++ main.js
@@ -0,0 +1,11 @@
+function showNav() {
+    document.getElementById("mob-navigation").style.display = "block";
+    document.getElementById("myToggle").style.display = "none";
+    document.getElementById("myToggleEnd").style.display = "block";
+}
+
+function hideNav() {
+    document.getElementById("mob-navigation").style.display = "none";
+    document.getElementById("myToggleEnd").style.display = "none";
+    document.getElementById("myToggle").style.display = "block";
+}
